d1813a5571a62c754cf8bfb776b5b6e06ac9d2bb
Em Grasmeder <emily@gras.org>
2023-04-20 00:28:01 +0200

    separate engine logic from game logic

--- Program.fs
+++ Program.fs
@@ -1,4 +1,6 @@
-﻿type Time = int
+﻿module Magic
+
+type Time = int
 type Id = int
 
 type Coordinate = int
@@ -7,8 +9,7 @@ type Coordinates = { X: Coordinate; Y: Coordinate }
 
 type Phenomenon =
     { Id: Id
-      Coordinates: Coordinates
-      Time: Time }
+      Coordinates: Coordinates}
 
 type Distance = int
 type Length = Distance
@@ -38,23 +39,4 @@ let translate (p: Phenomenon) (v: Vector) =
         Coordinates =
             { p.Coordinates with
                 X = p.Coordinates.X + v.ForceAppliedX
-                Y = p.Coordinates.Y + v.ForceAppliedY }
-        Time = p.Time + 1 }
-
-let debugLocation (p: Phenomenon) =
-    printfn $"X location: {p.Coordinates.X}"
-    printfn $"Y location: {p.Coordinates.Y}"
-    printfn $"Time: {p.Time}"
-
-let something =
-    { Coordinates = { X = 0; Y = 0 }
-      Time = 0
-      Id = 0 }
-
-let force =
-    { ForceAppliedX = 10
-      ForceAppliedY = 10 }
-
-debugLocation something
-let somethingChanged = translate something force
-debugLocation somethingChanged
+                Y = p.Coordinates.Y + v.ForceAppliedY }}
---
+++ SomeGame.fs
@@ -0,0 +1,19 @@
+﻿module magic.Magic.SomeGame
+
+open Magic
+
+let debugLocation (p: Phenomenon) =
+    printfn $"X location: {p.Coordinates.X}"
+    printfn $"Y location: {p.Coordinates.Y}"
+
+let something =
+    { Coordinates = { X = 0; Y = 0 }
+      Id = 0 }
+
+let force =
+    { ForceAppliedX = 10
+      ForceAppliedY = 10 }
+
+debugLocation something
+let somethingChanged = translate something force
+debugLocation somethingChanged
